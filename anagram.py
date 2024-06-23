745c1abfcfc7347f77db835e128afb8816b27f0a
ahmethamza3995 <42319712+ahmethamza3995@users.noreply.github.com>
2024-06-23 10:07:13 +0300

    Update anagram.py
    little improvement

--- anagram.py
+++ anagram.py
@@ -10,17 +10,23 @@ cur.execute("SELECT madde FROM madde")
 
 kelime = input("Anagramı kontrol edilecek kelimeyi giriniz: ").lower()
 
+sozluk_kelimesi_bulundu = False
 anagramlar = []
 for row in cur.fetchall():
     sozluk_kelimesi = row[0].lower()
-    if kelime != sozluk_kelimesi and are_anagrams(kelime, sozluk_kelimesi) and sozluk_kelimesi not in anagramlar:
+    if kelime == sozluk_kelimesi:
+        sozluk_kelimesi_bulundu = True
+    elif are_anagrams(kelime, sozluk_kelimesi) and sozluk_kelimesi not in anagramlar:
         anagramlar.append(sozluk_kelimesi)
 
-if anagramlar:
-    print("Girdiğiniz kelimenin anagramları:")
-    for anagram in anagramlar:
-        print(anagram)
+if sozluk_kelimesi_bulundu:
+    if anagramlar:
+        print("Girdiğiniz kelimenin anagramları:")
+        for anagram in anagramlar:
+            print(anagram)
+    else:
+        print("Girdiğiniz kelimenin sözlükte anagramı bulunamadı.")
 else:
-    print("Girdiğiniz kelimenin anagramı sözlükte bulunamadı.")
+    print("Girdiğiniz kelime sözlükte bulunamadı.")
 
 con.close()
